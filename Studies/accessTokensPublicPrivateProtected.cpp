9b5f2ae44e55f4c1ead207c7dda0f5ef7a39e90c
Enes Eken <144843964+eneseken95@users.noreply.github.com>
2025-01-03 18:47:28 +0300

    Update accessTokensPublicPrivateProtected.cpp

--- Studies/accessTokensPublicPrivateProtected.cpp
+++ Studies/accessTokensPublicPrivateProtected.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 class Nokta{
+    protected:
+    int p;
 
     private:
     int x;
@@ -9,22 +11,25 @@ class Nokta{
     public:
     int y;
     
-   //Protected:
-
 };
 
-
-
+class Nokta2 : public Nokta {
+    public:
+     void printP() {        
+        p = 20;
+        cout << "p: " << p << endl;
+    }
+};
 
 int main()
 {
-
     Nokta n1;
-
+    Nokta2 n2;
+    
     n1.y = 5;
-
     cout << n1.y << endl;
-
+    
+    n2.printP();
 
     return 0;
 }
